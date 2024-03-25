c05884b6099f1e26e95bc5c6a8d2d352caf80e77
WhoLook <51149853+NyanPunch@users.noreply.github.com>
2024-03-25 19:35:25 +0900

    HTTP request Test

---
+++ POST.py
@@ -0,0 +1,11 @@
+import requests
+
+# POST 요청에 사용할 데이터
+data = {'key1': 'value1', 'key2': 'value2'}
+
+# POST 요청 보내기
+response = requests.post('http://example.com/post_endpoint', data=data)
+
+# 응답 확인
+print('Status Code:', response.status_code)
+print('Response Body:', response.text)
